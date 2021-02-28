0c99c34d45b2c8d3b2a23737f0c721acc1a6635b
seunghun yang <yabby1997@gmail.com>
2021-02-28 16:52:58 +0900

    인터페이스 빌더

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var progressBar: UISlider!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+        print("Button Tapped")
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("Value Changed")
+    }
+    
 }
 
