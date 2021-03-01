7e02599e44576f98ab9d0be6b82b541405a99f83
seunghun yang <yabby1997@gmail.com>
2021-03-01 14:57:11 +0900

    구현코드 작성 1

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate{
 
+    var player: AVAudioPlayer!
+    var timer: Timer!
+    
     @IBOutlet weak var playPauseButton: UIButton!
     @IBOutlet weak var progressBar: UISlider!
     @IBOutlet weak var timeLabel: UILabel!
@@ -16,16 +20,95 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.initializePlayer()
     }
 
     @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
-        print("Button Tapped")
+        sender.isSelected = !sender.isSelected
+        
+        if sender.isSelected {
+            self.player?.play()
+            self.makeAndFireTimer()
+        } else {
+            self.player?.pause()
+            self.invalidateTimer()
+        }
     }
     
     @IBAction func sliderValueChanged(_ sender: UISlider) {
-        print("Value Changed")
+        self.updateTimeLabelText(time: TimeInterval(sender.value))
+        if sender.isTracking { return }
+        self.player.currentTime = TimeInterval(sender.value)
     }
     
+    func initializePlayer() {
+        guard let soundAsset: NSDataAsset = NSDataAsset(name: "sound") else {
+            print("음원 파일 에셋을 가져올 수 없습니다")
+            return
+        }
+        
+        do {
+            try self.player = AVAudioPlayer(data: soundAsset.data)
+            self.player.delegate = self
+        } catch let error as NSError {
+            print("플레이어 초기화 실패")
+            print("코드 : \(error.code), 메시지 : \(error.localizedDescription)")
+        }
+        
+        self.progressBar.maximumValue = Float(self.player.duration)
+        self.progressBar.minimumValue = 0
+        self.progressBar.value = Float(self.player.currentTime)
+    }
+    
+    func updateTimeLabelText(time: TimeInterval) {
+        let minute: Int = Int(time / 60)
+        let second: Int = Int(time.truncatingRemainder(dividingBy: 60))
+        let milisecond: Int = Int(time.truncatingRemainder(dividingBy: 1) * 100)
+        
+        let timeText: String = String(format: "%02ld:%02d:%02ld", minute, second, milisecond)
+        
+        self.timeLabel.text = timeText
+    }
+    
+    func makeAndFireTimer() {
+        self.timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { [unowned self] (timer: Timer) in
+            if self.progressBar.isTracking { return }
+            
+            self.updateTimeLabelText(time: self.player.currentTime)
+            self.progressBar.value = Float(self.player.currentTime)
+        })
+        self.timer.fire()
+    }
+    
+    func invalidateTimer() {
+        self.timer.invalidate()
+        self.timer = nil
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        guard let error: Error = error else {
+            print("오디오 플레이어 디코드 오류 발생")
+            return
+        }
+        
+        let message: String
+        message = "오디오 플레이어 오류 발생 \(error.localizedDescription)"
+        
+        let alert: UIAlertController = UIAlertController(title: "알림", message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        let okAction: UIAlertAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: { (action: UIAlertAction) -> Void in
+            self.dismiss(animated: true, completion: nil)
+        })
+        
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        self.playPauseButton.isSelected = false
+        self.progressBar.value = 0
+        self.updateTimeLabelText(time: 0)
+        self.invalidateTimer()
+    }
 }
 
